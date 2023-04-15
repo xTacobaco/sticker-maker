08b9d5bc939a12e431bbd484f76984d068215e66
william.beino <william.beino@hotmail.com>
2023-04-15 11:19:20 +0200

    stripes

--- main.js
+++ main.js
@@ -56,23 +56,30 @@ const stickerMaterial = new THREE.RawShaderMaterial({
     vertexShader,
 });
 const stickerOutlineMaterial = new THREE.MeshBasicMaterial({ color: 0xF9FEFF, alphaMap: alphaMap, transparent: true, side: THREE.DoubleSide});
+const stripedTexture = new THREE.TextureLoader().load('stripes.png');
+stripedTexture.repeat.set(0.5, 0.5);
+const backsideMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, map: stripedTexture, alphaMap: alphaMap, transparent: true, side: THREE.DoubleSide, opacity: 0.2});
 const plane = new THREE.Group();
 const stickermesh = new THREE.Mesh(roundrect, stickerMaterial);
 const outlinemesh = new THREE.Mesh(roundrect, stickerOutlineMaterial);
+const backsidemesh = new THREE.Mesh(roundrect, backsideMaterial);
+backsidemesh.position.z = -0.002;
 outlinemesh.position.z = -0.001;
 outlinemesh.scale.x = outlinemesh.scale.y = 1.06;
+backsidemesh.scale.x = backsidemesh.scale.y = 1.06;
 plane.add(stickermesh);
 plane.add(outlinemesh);
+plane.add(backsidemesh);
 const loader = new FontLoader();
 await loader.load( 'fonts/helvetiker_bold.typeface.json', function ( font ) {
 	const title = new TextGeometry( 'Sticker Name', {
 		font: font,
-		size: 0.05,
+		size: 0.06,
 		height: 0.001,
 	});
     title.center();
-    const titleMesh = new THREE.Mesh(title, new THREE.MeshBasicMaterial({ color: 0x323232 }));
-    titleMesh.position.z = -0.01;
+    const titleMesh = new THREE.Mesh(title, new THREE.MeshBasicMaterial({ color: 0x404040 }));
+    titleMesh.position.z = -0.002;
     titleMesh.position.y = 0.2;
     titleMesh.rotation.y = Math.PI;
     plane.add(titleMesh);
@@ -83,8 +90,8 @@ await loader.load( 'fonts/helvetiker_bold.typeface.json', function ( font ) {
 		height: 0.001,
 	});
     date.center();
-    const dateMesh = new THREE.Mesh(date, new THREE.MeshBasicMaterial({ color: 0x323232 }));
-    dateMesh.position.z = -0.01;
+    const dateMesh = new THREE.Mesh(date, new THREE.MeshBasicMaterial({ color: 0x404040 }));
+    dateMesh.position.z = -0.002;
     dateMesh.position.y = 0;
     dateMesh.rotation.y = Math.PI;
     plane.add(dateMesh);
